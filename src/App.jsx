4afc2572c41de38cc728721bbb524fb758fcbdc9
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-08-16 20:24:58 -0300

    Migração do HTML da página principal

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from "./images/logo.svg";
+
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="home center">
+      <div className="home__logo">
+        <img src={logo} className="responsive" alt="" />
+      </div>
+      <select className="home__select-users">
+        <option>User 1</option>
+        <option>User 2</option>
+        <option>User 3</option>
+      </select>
+      <button className="button-primary">Entrar</button>
+    </div>
+  );
+}
+
+export default App;
