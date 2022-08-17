7f0d4e2ac04493a3e2c08a02536d238e3f0d96a3
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-08-16 21:37:56 -0300

    Criação das páginas e roteamento completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
+import Users from "./components/pages/Users";
+import UserBlog from "./components/pages/UserBlog";
+import UserPost from "./components/pages/UserPost";
+import CreatePost from "./components/pages/CreatePost";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
@@ -11,6 +15,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/:userId/posts/:postId" element={<UserPost />} />
+        <Route path="/users/:userId/posts/new" element={<CreatePost />} />
       </Routes>
     </BrowserRouter>
   );
