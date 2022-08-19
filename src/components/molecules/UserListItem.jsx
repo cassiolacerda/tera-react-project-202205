21d66ee659aa62caea539eee23f3f82666574d3f
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-08-18 21:15:46 -0300

    Adiciona a página de Usuários

---
+++ src/components/molecules/UserListItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Avatar } from "../atoms";
+
+export default function UserListItem(props) {
+  return (
+    <Link to={`/users/${props.id}`} className="users__list-item">
+      <div className="users__list-item-photo">
+        <Avatar src={props.src} />
+      </div>
+      <div className="users__list-item-name">{props.name}</div>
+    </Link>
+  );
+}
