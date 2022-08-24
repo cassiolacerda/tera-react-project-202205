fc5687c6ac55766a952b4fc582811ac00354ead0
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-08-24 09:31:52 -0300

    Adiciona página inicial

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,56 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+import { Loading } from "../organisms";
 import logo from "../../images/logo.svg";
 
 export default function Home() {
-  return (
+  const navigate = useNavigate();
+
+  const [loggedUser, setLoggedUser] = React.useState(null);
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://62c4e487abea8c085a7e022a.mockapi.io/users/")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleUserChange = (event) => {
+    setLoggedUser(event.target.value);
+  };
+
+  const handleConfirmClick = () => {
+    navigate(`/users/${loggedUser}`);
+  };
+
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
-      <select className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+      <select className="home__select-users" onChange={handleUserChange}>
+        <option value="">Selecionar usuário</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option
+              key={user.id}
+              value={user.id}
+            >{`${user.fn} ${user.ln}`}</option>
+          ))}
       </select>
-      <button className="button-primary">Entrar</button>
+      {loggedUser && (
+        <button onClick={handleConfirmClick} className="button-primary">
+          Entrar
+        </button>
+      )}
     </div>
   );
 }
